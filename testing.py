3f2b0e561f66b815e6ec73807004a15c18f181ac
gpffhajdzl <gpffhajdzl@naver.com>
2023-10-09 17:00:42 +0900

    딥러닝 과제중...

---
+++ testing.py
@@ -0,0 +1,14 @@
+# import numpy as np
+#
+# a = np.array([0,1,2,3])
+# b=a.reshape((2,-1))
+# c = a.reshape(-1)
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# print(a.shape)
+# print(b.shape)
+# print(c.shape)
+
